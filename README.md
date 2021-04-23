61e4e7cfc5bce2a41eea1b6308aed471f2412265
Aaron Van De Brook <avandebrook5@gmail.com>
2021-04-23 01:18:58 -0400

    Update gitignore, readme. Add system to parse command line arguments.

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # DES Implementation
 School related project (Applied Cryptography) to implement DES in any programming language.
+# Build
+Project is configured with [cmake](https://cmake.org/). Use the following to build the project:
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
+# Run
+Build the project first and run one of the following:
+```bash
+$ ./des
+Usage: ./des [encrypt|decrypt] [key] [file|string]
+```
+# Development Tools
+## Build Tools
+The following tools/versions are/were used to develop this project:
+* CMake:
+```
+$ cmake --version
+cmake version 3.19.7
+```
+* GCC:
+```
+$ gcc --version
+gcc (GCC) 10.2.1 20201125 (Red Hat 10.2.1-9)
+```
+* Make:
+```
+$ make --version
+GNU Make 4.3
+Built for x86_64-redhat-linux-gnu
+```
+## Operating System
+* OS: Fedora 33 (Workstation Edition) x86_64
+* Kernel: Linux 5.11
--- src/main.c
+++ src/main.c
@@ -1,14 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "main.h"
 
 int main(int argc, char **argv)
 {
-    if (argc < 3) {
-        printf("Usage: %s [encrypt|decrypt] [key] [file|string]\n", argv[0]);
-        return 1;
-    }
+	program_info_t cmd_args;
+	if (argc < REQUIRED_ARGS) {
+		fprintf(stderr, "Usage: %s [encrypt|decrypt] [key] [-f <file> | -s <string>]\n", argv[0]);
+		return 1;
+	}
 
-    return 0;
+	if (!parse_args(&cmd_args, argv)) {
+		printf("correct input\n");
+	}
+
+	return 0;
+}
+
+int parse_args(program_info_t *cmd_info, char **argv)
+{
+	int i, status_code = 0;
+	FILE *data_file;
+
+	if (strcmp(argv[1], "encrypt") == 0) {
+		cmd_info->encrypt_or_decrypt = SCHEME_ENCRYPT;
+	} else if (strcmp(argv[1], "decrypt") == 0) {
+		cmd_info->encrypt_or_decrypt = SCHEME_DECRYPT;
+	} else {
+		status_code |= SET_BIT(0);
+	}
+
+	for (i = 0; i < DES_KEY_LENGTH_BYTES && argv[2][i] != 0; i++) {
+		cmd_info->key[i] = argv[2][i];
+	}
+
+	if (strcmp(argv[3], "-f") == 0) {
+		cmd_info->file_or_string = DATA_TYPE_FILE;
+	} else if (strcmp(argv[3], "-s") == 0) {
+		cmd_info->file_or_string = DATA_TYPE_STRING;
+	} else {
+		status_code |= SET_BIT(2);
+	}
+
+	if (cmd_info->file_or_string == DATA_TYPE_FILE) {
+		if (access(argv[4], F_OK)) {
+			data_file = fopen(argv[4], "rb");
+			fread(cmd_info->data, sizeof(char), MAX_DATA_LENGTH, data_file);
+			fclose(data_file);
+		} else {
+			SET_BIT(3);
+		}
+	} else if (cmd_info->file_or_string == DATA_TYPE_STRING) {
+		for (i = 0; i < MAX_DATA_LENGTH && argv[4] != 0; i++) {
+			cmd_info->data[i] = argv[4][i];
+		}
+	} else {
+		SET_BIT(3);
+	}
+
+	return status_code;
 }
